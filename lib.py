01c3fcff5da9ae3dce76db23fd4dde17d6460e73
cynic <kurisufag1@gmail.com>
2022-10-27 16:25:30 -0400

    double gomen

--- lib.py
+++ lib.py
@@ -68,7 +68,7 @@ def execute(program):
             elif ctx[0] == _ident("miracle"):
                 return _box(getattr(eval(_destr(subs[1])), _destr(subs[2]))(*[(i if type(i) == type([]) else i[1]) for i in _fixarr(subs[3])]))
             elif ctx[0] == _ident("r"):
-                return open(_destr(subs[1]), "r").read(int(_destr(subs[2])))
+                return open(_destr(subs[1]), "r").read(subs[2][1])
             elif ctx[0] == _ident("w"):
                 return open(_destr(subs[1]), "w").write(_destr(subs[2]))
             elif ctx[0] == _ident("def"):
